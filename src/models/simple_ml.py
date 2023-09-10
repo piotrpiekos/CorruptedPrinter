8ee223edce24eee306e68c10afaee308a1348920
piotrpiekos <piotrpiekos@gmail.com>
2023-09-11 00:53:54 +0300

    implements script for the evaluation

---
+++ src/models/simple_ml.py
@@ -0,0 +1,10 @@
+class DirectKNN:
+    def __init__(self, k):
+        pass
+
+    def adapt_to_traj(self, T):
+        pass    
+
+    def __call__(self, *args, **kwargs):
+        # get a prediction of the model
+        pass
